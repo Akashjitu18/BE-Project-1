f5f05b480c869e8d35f3cf9e919b0a9f4277c303
AkashJitu18 <akashjitu18@gmail.com>
2025-11-24 17:35:38 +0530

    add user registration route, implement user controller with register endpoint, fix asyncHandler return statement and password hashing typo

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
   };
 };
@@ -18,7 +18,7 @@ export default asyncHandler;
 /*
 const asyncHandler = (fn) => async (req, res, next) => {
     try {
-        await fn(req, res, next);
+       return await fn(req, res, next);
     } catch (error) {
         res.status(error.code || 500).json({
             success: false,
